823e7b28bd16cabeffa56325f2b0cdfa6ea7fe45
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2025-12-10 13:18:05 -0700

    default page

---
+++ news-app/components/news-list.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+interface NewsItem{
+    id: string; 
+    slug: string;
+    title: string;
+    image: string;
+    date: string;
+    content: string;
+}
+interface NewsProps{
+    news: NewsItem[];
+}
+export default function NewsList({news}: NewsProps) {
+  return (
+    <ul className="news-list">
+      {news.map((newsItem) => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            <span>{newsItem.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
