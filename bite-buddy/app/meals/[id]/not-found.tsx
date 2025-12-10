14a713348ba0dbb6a0a12e13f5fab405d125d774
Rakesh singh <rakeshsingh@mac.mynetworksettings.com>
2025-12-09 20:50:09 -0700

    added

--- bite-buddy/app/meals/[id]/not-found.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-export default function NotFound(){
-    return <main className="not-found">
-        <h1>Meal not found</h1>
-    </main>
-}
--- bite-buddy/app/not-found.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-export default function NotFound(){
-    return <main className="not-found">
-        <h1>Resource u are looking for is not availale</h1>
-    </main>
-}
