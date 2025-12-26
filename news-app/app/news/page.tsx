9995069fa0baca59aee3ce7ed0017f77161462c8
Rakesh singh <rakeshsingh@mac.mynetworksettings.com>
2025-12-26 10:05:56 -0700

    data fetching- client side

--- news-app/app/news/page.tsx
+++ news-app/app/news/page.tsx
@@ -1,11 +1,43 @@
+'use client'
 import NewsList from '@/components/news-list';
-import { DUMMY_NEWS } from '@/dummy-news';
+import { useEffect, useState } from 'react';
 
 export default function NewsPage() {
+  const [error,setError] = useState<string | null>();
+  const [isLoading,setIsLoading] = useState<Boolean|null>(false);
+  const [news, setNews] = useState();
+  useEffect(() =>{
+    async function fetchNews(){
+      setIsLoading(true)
+    const response = await fetch('http://localhost:8080/news');
+    if(!response.ok){
+      setError('Error occurred while fetching data')
+      setIsLoading(false);
+    }
+    const news = await response.json();
+    setIsLoading(false)
+    console.log(news)
+    setNews(news)
+    }
+    fetchNews()
+  },[]);
+
+  if (isLoading){
+    return <p> Loading .... </p>
+  }
+  if(error){
+    return <p>{error}</p>
+  }
+
+  let newsContent;
+  if(news){
+    newsContent = <NewsList news={news} />
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
+      
     </>
   );
 }
