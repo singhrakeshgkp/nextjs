7d1f481a378a59d2fd483365f317be560c45eb09
Rakesh singh <rakeshsingh@mac.mynetworksettings.com>
2026-01-14 20:21:59 -0700

    dsd

--- page-router-ex/pages/clients/[id]/index.js
+++ page-router-ex/pages/clients/[id]/index.js
@@ -1,3 +1,20 @@
+import { useRouter } from "next/router";
+
 export default function clientProjects(){
-    return <><h1>Projects by Client id page</h1></>
+    const router = useRouter();
+    function loadProjectHandler(){
+        //.... code to fetch data from api or db
+
+        // approach 1
+        /* router.push('/clients/001/projecta') */ 
+        // approach 2 by passing object to push method
+        router.push({
+            pathname: "/clients/[id]]/[clientPrjectId]",
+            query: {id: "002",clientProjectId: "projectA"}
+        });
+    }
+    return <><h1>Projects by Client id page</h1>
+
+    <button onClick={loadProjectHandler}>Load Project A</button>
+    </>
 }
